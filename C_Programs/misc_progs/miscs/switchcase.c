ee2e121a5929dd62cc1cbd00d7eb2493c3cfd647
Umesh Patil <umeshp@cdac.in>
2021-02-09 15:20:12 +0530

    added misc template c prog

---
+++ C_Programs/misc_progs/miscs/switchcase.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int i = 3;
+	
+	switch(i)
+	{
+		case 0+1 :
+		printf("coding");
+		break;
+		
+		case 1+2 :
+		printf("Quiz");
+		break;
+
+		default:
+		printf("CodeingQuiz");
+	}
+	return 0;
+
+}
